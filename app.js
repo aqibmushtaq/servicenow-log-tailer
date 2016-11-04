10817233ab30ca2441b08c8e977dee7d6bd9e49a
Aqib Mushtaq <aqib@mushtaq.me>
2016-11-04 20:10:18 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var request = require("request");
+var parseString = require('xml2js').parseString;
+var querystring = require('querystring');
+var zlib = require('zlib');
+var properties = require('./properties');
+var args = process.argv.slice(2);
+
+var cookiesArr = [];
+for (var cookieName in properties['cookie']) {
+    cookiesArr.push(cookieName+'='+properties['cookie'][cookieName]);
+}
+var cookiesStr = cookiesArr.join('; ');
+
+var options = {
+    method: 'POST',
+    url: properties.host + '/xmlhttp.do',
+    headers:
+    {
+        'Accept-Encoding':'gzip, deflate, br',
+        'Accept-Language':'en-US,en;q=0.8,en-GB;q=0.6',
+        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie':cookiesStr,
+        'X-UserToken': properties.sessionToken,
+    }
+};
+var form = {
+    'ni.nolog.x_referer'            : 'ignore',
+    'sysparm_express_transaction'   : 'true',
+    'sysparm_name'                  : 'logtail',
+    'sysparm_processor'             : 'ChannelAjax',
+    'sysparm_scope'                 : 'global',
+    'sysparm_silent_request'        : 'true',
+    'sysparm_type'                  : 'read',
+    'sysparm_value'                 : '',
+    'sysparm_want_session_messages' : 'true',
+    'x_referer'                     : 'channel.do?sysparm_channel=logtail'
+};
+
+var lastSeq = '-1';
+function printLogs(logs) {
+    parseString(logs, function (err, result) {
+        lastSeq = result['xml']['$']['channel_last_sequence'];
+        var logs = result['xml']['item'];
+        logs.forEach((log) => {
+            console.log(log['$']['message']);
+        });
+        getLogs();
+    });
+}
+function getLogs() {
+    if (lastSeq != '') {
+        form.sysparm_value = lastSeq
+    }
+
+    var formData = querystring.stringify(form);
+    var contentLength = formData.length;
+    options['headers']['Content-Length'] = contentLength;
+    options['body'] = formData;
+
+    var response = request(options);
+    var gunzip = zlib.createGunzip();
+    var logs = "";
+    gunzip.on('data', function(data){
+        logs += data.toString();
+    });
+    gunzip.on('end', function(){
+        printLogs(logs)
+    });
+    response.pipe(gunzip);
+}
+getLogs();
---
+++ properties.js
@@ -0,0 +1,8 @@
+module.exports = {
+    cookie: {
+        'JSESSIONID' : '',
+        'BIGipServerpool_[insert instance name here]' : ''
+    },
+    sessionToken: '',
+    host: ''
+}
